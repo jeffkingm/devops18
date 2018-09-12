fe50234f755a3f2536c2c6656fae7547b390d951
jeffkingm <42356087+jeffkingm@users.noreply.github.com>
2018-09-12 14:57:00 +0200

    Update todo.py

--- v35/tis/todo.py
+++ v35/tis/todo.py
@@ -17,7 +17,7 @@
 *    { git push -u origin master } : Instruktion till git att lägga upp innehållet i katalogen till git repository
 *
 *    { git clone[url:n-till-git-repositoryn] } : Klona en git repository lokalt i din dator
-  
+*
 *     Kontrollera i din git repository att allt lades upp 
 *  
 *    UTVECKLINGSMILJÖ FÖR PYTHON
